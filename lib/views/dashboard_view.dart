a79ae455cd9e7a34da27d02fc33c0698180bfe72
Carlos Villalobos <cvillalobos@konradgroup.com>
2022-11-28 00:25:10 -0600

    [FSBM] - Add looping beat logic

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:workshop/widgets/beat_controller_widget.dart';
 
@@ -18,6 +20,8 @@ class DashboardViewState extends State<DashboardView> {
 
   // Every class variable that we define within our State class global scope
   // will be part of our view state.
+  int _beatNote = 0;
+
   bool _beatPlaying = false;
 
   final List _notes = [
@@ -46,6 +50,45 @@ class DashboardViewState extends State<DashboardView> {
         pillsState: _notes[index],
         controllerIndex: index,
         updateControllerState: _onControllerPillCLick
+    );
+  }
+
+  // StatefulWidgets have an special initial state set up method, we can use it
+  // to start our playing loop and just verify if our _beatPlaying flag is on
+  // or off.
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(milliseconds: 300), (timer) {
+      if (_beatPlaying) {
+        // If a current note is true then play the respective sound, the
+        // AudioPlayers library assumes by default that our resources are
+        // located within an assets folder at the root of our project.
+        if (_notes[0][_beatNote]) _playSound("snd_kick_drum.wav");
+        if (_notes[1][_beatNote]) _playSound("snd_snare_drum.wav");
+        if (_notes[2][_beatNote]) _playSound("snd_hi_hat.wav");
+
+        _beatNote += 1;
+
+        // We only have 4 pills, so if we reach the end of the list, we
+        // need to start again
+        if (_beatNote == 4) _beatNote = 0;
+      }
+    });
+  }
+
+  /// Flutter allows asynchronous code to be run without stopping the UI
+  /// thread by applying an async modifier to a method.
+  void _playSound(String soundResource) async {
+    final audioPlayer = AudioPlayer();
+
+    // All asynchronous execution must have an await in order
+    // for it to work.
+    //
+    // The audioplayers package has a low latency playing mode.
+    await audioPlayer.play(
+        AssetSource(soundResource),
+        mode: PlayerMode.lowLatency
     );
   }
 
