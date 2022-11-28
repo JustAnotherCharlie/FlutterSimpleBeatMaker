020373c03a3448421b8deb7365dbd8c2be04f832
Carlos Villalobos <cvillalobos@konradgroup.com>
2022-11-27 21:55:50 -0600

    [FSBM] - Set up base project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
+// Import Flutter required package
 import 'package:flutter/material.dart';
 
+// Dart code entry function
 void main() {
+  // Initialize a Widget as our application, remember everything within
+  // flutter can be seen as a Widget (Text, Container, Button, etc.) even
+  // our main application object will be a Widget.
   runApp(const MyApp());
 }
 
+// Our main application object, which extends the StatelessWidget class since
+// the main app container won't change state at any given time.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+  // Please review the following links to learn more about the
+  // implemented widget:
+  //
+  // MaterialApp - https://api.flutter.dev/flutter/material/MaterialApp-class.html
+  // Scaffold - https://api.flutter.dev/flutter/material/Scaffold-class.html
+  // SafeArea - https://api.flutter.dev/flutter/widgets/SafeArea-class.html
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: Text("My First Flutter Application"),
+      home: Scaffold(
+        body: SafeArea(
+          child: Text("My First Flutter Application")
+        ),
+      ),
     );
   }
 }
